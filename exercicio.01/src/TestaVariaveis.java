28edb1e660f81dd9bca0733320bda1eb2059c4b1
suely <suelyribeirocm@gmail.com>
2019-08-19 21:00:23 -0300

    Ajustando alguns erros

---
+++ exercicio.01/src/TestaVariaveis.java
@@ -0,0 +1,15 @@
+
+public class TestaVariaveis {
+
+	public static void main( String [] args) {
+		String nome = "Luana";
+		System.out.println(nome);
+		int idade = 30;
+		idade += (7 * 5 + 2);
+		System.out.print(idade + "\n");
+		System.out.println( nome + " tem "+ idade + " anos.");
+		double salario = 1270.50;
+		long valor = 576896596907661l;
+		float salario2 = 3.14f;			
+	}
+}
